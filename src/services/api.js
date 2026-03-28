4286c419228a0c7446be670b737b2140ded75a32
gabriel-hsoares <soaresgabriel1@icloud.com>
2026-03-28 15:37:10 -0300

    atualizacao api para prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000'
 });
 
-
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
 
   if (token) {
@@ -13,6 +12,6 @@ api.interceptors.request.use(config => {
   }
 
   return config;
-}); 
+});
 
 export default api;
